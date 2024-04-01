02697bf7cc37c4fcee9248e02aa9d094b361852c
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-04-01 12:43:35 +0200

    add sass builder

---
+++ README.md
@@ -0,0 +1,8 @@
+# Animalife
+
+```bash
+symfony server start -d
+php bin/console sass:build --watch
+npm run watch / dev
+
+```
--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfonycasts\SassBundle\SymfonycastsSassBundle::class => ['all' => true],
 ];
