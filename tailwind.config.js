807b5702a4231a8080ca2733d6d8ea78a6911ad8
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-04-07 11:16:14 +0200

    fix media

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      '2xl': {'max': '1400px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1199px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '992px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '576px'},
+      // => @media (max-width: 639px) { ... }
+    }
   },
   plugins: [],
 }
