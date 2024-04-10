1132c1678aeca70154bfd6f7d3187e1740ce7d9d
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-04-10 22:35:20 +0200

    static pages route

---
+++ src/Controller/StaticPageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StaticPageController extends AbstractController
+{
+    #[Route('/mentions-legales', name: 'mentions-legales')]
+    public function legalMentions(): Response
+    {
+        return $this->render('static_pages/legal_mentions.html.twig');
+    }
+
+    #[Route('/politique-de-confidentialite', name: 'politique-de-confidentialite')]
+    public function privacyPolicy(): Response
+    {
+        return $this->render('static_pages/privacy_policy.html.twig');
+    }
+}
