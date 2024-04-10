a9f930889ae6b1297e5936c15c83c619ec80e736
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-04-10 22:09:10 +0200

    add exception to send to 404 when unknown page loaded

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    private $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof NotFoundHttpException) {
+            $url = $this->urlGenerator->generate('error404');
+            $response = new RedirectResponse($url);
+
+            $event->setResponse($response);
+        }
+    }
+}
