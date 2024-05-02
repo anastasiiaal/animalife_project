95acb95b0d95d76cc91bc41ca9829842df2e293c
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2024-05-02 19:11:46 +0200

    add .param elements & handle click in js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,2 +1,12 @@
-// js here
-console.log("test");
+// function that handles click on .param elements to update query and doctor list
+document.addEventListener('DOMContentLoaded', function () {
+    let params = document.querySelectorAll('.param');
+    params.forEach(function(param) {
+        param.addEventListener('click', function() {
+            let paramName = this.getAttribute('data-param');
+            let url = new URL(window.location);
+            url.searchParams.delete(paramName);
+            window.location.href = url;
+        });
+    });
+});
